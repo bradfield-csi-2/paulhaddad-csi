15ac554ca33520963f311162b0c36024cd6a1764
Paul Haddad <paulh16@gmail.com>
2021-04-01 14:24:19 -0400

    Intro to Assembly Programming
    * Finish out Pangram

--- computer-systems/x86-64-programming/binary_convert.asm
+++ computer-systems/x86-64-programming/binary_convert.asm
@@ -2,17 +2,16 @@ section .text
 global binary_convert
 binary_convert:
 	xor rax, rax
-	xor r9, r9
 
 .loop:
-	movzx r8, byte [rdi+r9] ; get leading byte
+	movzx r8, byte [rdi]    ; get leading byte
 	cmp r8, 0               ; exit if null char
 	je .done
 	sal rax, 1              ; running total * 2
 	sub r8, '0'             ; get numeric digit
 	add rax, r8             ; add digit to running total
-	inc r9
-jmp .loop
+	inc rdi                 ; increment address of string
+	jmp .loop
 
 .done:
 	ret
