2c7a9bbb73cad7e64884a36ee30a1d8adfd3b8fd
Paul Haddad <paulh16@gmail.com>
2021-04-06 13:42:13 -0400

    Move vec_length to a single call outside the loop
    * Reduced from 9ns to ~4.5ns

--- computer-systems/low-level-optimization/vec/dotproduct.c
+++ computer-systems/low-level-optimization/vec/dotproduct.c
@@ -2,16 +2,16 @@
 
 
 data_t dotproduct(vec_ptr u, vec_ptr v) {
-   data_t sum = 0, u_val, v_val;
+  data_t sum = 0, u_val, v_val;
 
-   for (long i = 0; i < vec_length(u); i++) { // we can assume both vectors are same length
-        if (i < 0 || i >= u->len)
-          return sum;
+  long length = vec_length(u); // we're assuming u and v are the same lengths
 
-        u_val = u->data[i];
-        v_val = v->data[i];
+  for (long i = 0; i < length; i++) { // we can assume both vectors are same length
 
-        sum += u_val * v_val;
-   }
-   return sum;
+    u_val = u->data[i];
+    v_val = v->data[i];
+
+    sum += u_val * v_val;
+  }
+  return sum;
 }
