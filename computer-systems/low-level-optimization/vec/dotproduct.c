dfd60212f21495b2ba8517f3307a23767b229eb0
Paul Haddad <paulh16@gmail.com>
2021-04-07 14:03:07 -0400

    Dotproduct: Implement 2 x 1 loop unrolling

--- computer-systems/low-level-optimization/vec/dotproduct.c
+++ computer-systems/low-level-optimization/vec/dotproduct.c
@@ -2,16 +2,30 @@
 
 
 data_t dotproduct(vec_ptr u, vec_ptr v) {
-  data_t sum = 0, u_val, v_val;
+  data_t sum = 0, u_val1, v_val1, u_val2, v_val2;
 
   long length = vec_length(u); // we're assuming u and v are the same lengths
+  int limit = length-1; // unrolling factor
+  long i;
 
-  for (long i = 0; i < length; i++) { // we can assume both vectors are same length
+  for (i = 0; i < limit; i+=2) { // we can assume both vectors are same length
 
-    u_val = u->data[i];
-    v_val = v->data[i];
+    u_val1 = u->data[i];
+    v_val1 = v->data[i];
 
-    sum += u_val * v_val;
+    u_val2 = u->data[i+1];
+    v_val2 = v->data[i+1];
+
+    sum += u_val1 * v_val1;
+    sum += u_val2 * v_val2;
   }
+
+  for (; i < length; i++) {
+    u_val1 = u->data[i];
+    v_val1 = v->data[i];
+
+    sum += u_val1 * v_val1;
+  }
+
   return sum;
 }
