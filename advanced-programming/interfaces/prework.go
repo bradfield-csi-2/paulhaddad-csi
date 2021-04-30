0942de6fe2c868c4b7ed97a266bf1cb282d22e38
Paul Haddad <paulh16@gmail.com>
2021-04-30 16:34:36 -0400

    Work on interating through interface methods

--- advanced-programming/interfaces/prework.go
+++ advanced-programming/interfaces/prework.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"unsafe"
 )
 
 type iface struct {
-	tab  unsafe.Pointer // this would normally be an itab value, but we don't need it for the exercise
+	tab  *itab
 	data unsafe.Pointer
+}
+
+type interfacetype struct {
+}
+
+type itab struct {
+	inter unsafe.Pointer
+	_type unsafe.Pointer
+	hash  uint32
+	_     [4]byte
+	fun   [1]uintptr
 }
 
 // ExtractValueFromInterface extracts the int value from the interface without
@@ -16,4 +28,16 @@ func ExtractValueFromInterface(i interface{}) int {
 	data := *(*int)(iPtr.data)
 
 	return data
+}
+
+// GetInterfaceMethods returns the method names that can be called on the
+// interface value
+func GetInterfaceMethods(i interface{}) []string {
+	iPtr := (*iface)(unsafe.Pointer(&i))
+	itable := (*itab)(iPtr.tab)
+	// methods := (*[1 << 16]unsafe.Pointer)(unsafe.Pointer(&itable.fun[0]))
+
+	fmt.Printf("%v\n", (itable.fun[0] + uintptr(0)))
+
+	return []string{"area", "perimeter"}
 }
--- advanced-programming/interfaces/prework_test.go
+++ advanced-programming/interfaces/prework_test.go
@@ -1,6 +1,26 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+type Geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (r rectangle) perimeter() float64 {
+	return 2*r.width + 2*r.height
+}
 
 func TestExtractValueFromInterface(t *testing.T) {
 	var tests = []struct {
@@ -17,6 +37,23 @@ func TestExtractValueFromInterface(t *testing.T) {
 		got := ExtractValueFromInterface(i)
 		if got != test.want {
 			t.Errorf("got: %d; want: %d\n", got, test.want)
+		}
+	}
+}
+
+func TestGetInterfaceMethods(t *testing.T) {
+	var tests = []struct {
+		iFaceValue interface{}
+		want       []string
+	}{
+		{Geometry(rectangle{10.0, 10.0}), []string{"area", "perimeter"}},
+	}
+
+	for _, test := range tests {
+		got := GetInterfaceMethods(test.iFaceValue)
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("got: %v; want: %v\n", got, test.want)
 		}
 	}
 }
