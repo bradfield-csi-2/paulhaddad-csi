5d57a7b3dd5c7e9426d0dd7c148cde336b6f1c42
Paul Haddad <paulh16@gmail.com>
2021-06-15 06:40:25 -0400

    HTTP Proxy Lab
    * Implement complete request and response in the browser

--- computer-networks/http/proxy/main.go
+++ computer-networks/http/proxy/main.go
@@ -11,8 +11,8 @@ func main() {
 		fmt.Println(err)
 	}
 
-	addr := &syscall.SockaddrInet4{Port: 8000}
-	err = syscall.Bind(fd, addr)
+	proxyAddr := &syscall.SockaddrInet4{Port: 8000}
+	err = syscall.Bind(fd, proxyAddr)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -29,15 +29,50 @@ func main() {
 
 	fmt.Printf("%v %v\n", nfd, sa)
 
-	buf := make([]byte, 100)
+	buf := make([]byte, 1024)
 	n, from, err := syscall.Recvfrom(nfd, buf, 0)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("Received:", n, from)
+	fmt.Println("Received:", n, from, buf)
 
-	err = syscall.Sendto(nfd, buf, 0, addr)
+	// connect to backing server and send message
+	sfd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	servAddr := &syscall.SockaddrInet4{Port: 9000}
+	err = syscall.Connect(sfd, servAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = syscall.Sendto(sfd, buf, 0, servAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rBuf := make([]byte, 1024)
+	n, from, err = syscall.Recvfrom(sfd, rBuf, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Received from server: %s\n\n", rBuf)
+
+	err = syscall.Sendto(nfd, rBuf, 0, proxyAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = syscall.Close(sfd)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = syscall.Close(nfd)
 	if err != nil {
 		fmt.Println(err)
 	}
