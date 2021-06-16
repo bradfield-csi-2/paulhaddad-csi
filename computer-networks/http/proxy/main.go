3096e2994ec5878f204f1538ea1a58153b5eb3c4
Paul Haddad <paulh16@gmail.com>
2021-06-16 07:15:39 -0400

    Refactor program

--- computer-networks/http/proxy/main.go
+++ computer-networks/http/proxy/main.go
@@ -3,8 +3,17 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"syscall"
 )
+
+const (
+	proxyPort  = 8000
+	serverPort = 9000
+)
+
+var cachePaths = [][]byte{[]byte("website/")}
+var cachedReqs = make(map[string][]byte)
 
 func closeSocket(fd int) {
 	err := syscall.Close(fd)
@@ -13,103 +22,39 @@ func closeSocket(fd int) {
 	}
 }
 
-var cachePaths = [][]byte{[]byte("website/")}
-var cachedReqs = make(map[string][]byte)
+func parsePath(buf []byte) []byte {
+	reqMsg := bytes.Split(buf, []byte("\n"))
+	reqLine := bytes.Split(reqMsg[0], []byte(" "))
+	return reqLine[1][1:]
+}
 
-func forwardReq(req []byte) []byte {
+func forwardReq(req []byte) ([]byte, error) {
+	var rBuf []byte = make([]byte, 1024)
+
 	sfd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("error creating socket: %s", err)
 	}
 
-	servAddr := &syscall.SockaddrInet4{Port: 9000}
+	defer closeSocket(sfd)
+
+	servAddr := &syscall.SockaddrInet4{Port: serverPort}
 	err = syscall.Connect(sfd, servAddr)
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("error connecting to socket: %s", err)
 	}
 
 	err = syscall.Sendto(sfd, req, 0, servAddr)
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("error sending to proxy: %s", err)
 	}
 
-	rBuf := make([]byte, 1024)
 	n, _, err := syscall.Recvfrom(sfd, rBuf, 0)
 	if err != nil {
-		fmt.Println(err)
+		return nil, fmt.Errorf("error receiving from proxy: %s", err)
 	}
 
-	err = syscall.Close(sfd)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return rBuf[:n]
-}
-
-func main() {
-	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer closeSocket(fd)
-
-	proxyAddr := &syscall.SockaddrInet4{Port: 8000}
-	err = syscall.Bind(fd, proxyAddr)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = syscall.Listen(fd, 10)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	var resp []byte
-	for {
-		nfd, _, err := syscall.Accept(fd)
-		if err != nil {
-			fmt.Println("accepting:", err)
-		}
-
-		buf := make([]byte, 1024)
-		n, _, err := syscall.Recvfrom(nfd, buf, 0)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		reqMsg := bytes.Split(buf[:n], []byte("\n"))
-		reqLine := bytes.Split(reqMsg[0], []byte(" "))
-
-		path := reqLine[1][1:]
-		fmt.Println("Path: ", path)
-
-		searchCache := matchesCachePath(path)
-		cachedRes, found := cachedReqs[string(path)]
-
-		switch {
-		case searchCache && found:
-			fmt.Println("Found in cache")
-			resp = cachedRes
-		case searchCache && !found:
-			fmt.Println("Need to cache")
-			resp = forwardReq(buf[:n])
-			cachedReqs[string(path)] = resp
-		default:
-			fmt.Println("Bypass cache")
-			resp = forwardReq(buf[:n])
-		}
-
-		err = syscall.Sendto(nfd, resp, 0, proxyAddr)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		err = syscall.Close(nfd)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+	return rBuf[:n], nil
 }
 
 func matchesCachePath(path []byte) bool {
@@ -120,4 +65,88 @@ func matchesCachePath(path []byte) bool {
 	}
 
 	return false
+}
+
+func createProxyResp(resp []byte) []byte {
+	proxyRespHdr := bytes.Split(resp, []byte("\r\n\r\n"))[0]
+	proxyRespData := bytes.Split(resp, []byte("\r\n\r\n"))[1]
+
+	updatedHeader := make([]byte, 0)
+	updatedHeader = append(updatedHeader, proxyRespHdr...)
+
+	// add keep-alive header for persistant connection
+	updatedHeader = append(updatedHeader, []byte("\r\nConnection: Keep-Alive\r\n\r\n")...)
+
+	proxiedResp := make([]byte, 0)
+	proxiedResp = append(proxiedResp, updatedHeader...)
+	proxiedResp = append(proxiedResp, proxyRespData...)
+
+	return proxiedResp
+}
+
+func main() {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		fmt.Println("error connecting to socket:", err)
+	}
+	defer closeSocket(fd)
+
+	proxyAddr := &syscall.SockaddrInet4{Port: proxyPort}
+	err = syscall.Bind(fd, proxyAddr)
+	if err != nil {
+		fmt.Println("error binding to socket:", err)
+	}
+
+	err = syscall.Listen(fd, 10)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		var proxyResp []byte
+
+		nfd, _, err := syscall.Accept(fd)
+		if err != nil {
+			log.Fatal("error accepting:", err)
+		}
+
+		defer closeSocket(nfd)
+
+		buf := make([]byte, 1024)
+		n, _, err := syscall.Recvfrom(nfd, buf, 0)
+		if err != nil {
+			log.Fatal("error receiving from client:", err)
+		}
+
+		path := parsePath(buf[:n])
+
+		searchCache := matchesCachePath(path)
+		cachedRes, found := cachedReqs[string(path)]
+
+		switch {
+		case searchCache && found:
+			log.Println("Found in cache")
+			proxyResp = cachedRes
+		case searchCache && !found:
+			log.Println("Need to cache")
+			serverResp, err := forwardReq(buf[:n])
+			if err != nil {
+				log.Fatal("error encountered forwarding request")
+			}
+			proxyResp = createProxyResp(serverResp)
+			cachedReqs[string(path)] = proxyResp
+		default:
+			log.Println("Bypass cache")
+			serverResp, err := forwardReq(buf[:n])
+			if err != nil {
+				log.Fatal("error encountered forwarding request")
+			}
+			proxyResp = createProxyResp(serverResp)
+		}
+
+		err = syscall.Sendto(nfd, proxyResp, 0, proxyAddr)
+		if err != nil {
+			log.Fatal("error sending to client:", err)
+		}
+	}
 }
