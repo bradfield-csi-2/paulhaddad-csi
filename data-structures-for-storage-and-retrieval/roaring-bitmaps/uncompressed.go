f5c7a7baaa435c4c2680d93fd39e26c374560451
Paul Haddad <paulh16@gmail.com>
2021-08-02 08:02:09 -0400

    Implement Get and Set

---
+++ data-structures-for-storage-and-retrieval/roaring-bitmaps/uncompressed.go
@@ -0,0 +1,43 @@
+package bitmap
+
+import (
+	"math"
+)
+
+const wordSize = 64
+
+type uncompressedBitmap struct {
+	data []uint64
+}
+
+func newUncompressedBitmap() *uncompressedBitmap {
+	return &uncompressedBitmap{
+		data: make([]uint64, math.MaxUint32/wordSize),
+	}
+}
+
+func (b *uncompressedBitmap) Get(x uint32) bool {
+	index := x / 64
+	offset := x % 64
+	return b.data[index]&(1<<offset) > 0
+}
+
+func (b *uncompressedBitmap) Set(x uint32) {
+	index := x / 64
+	offset := x % 64
+	b.data[index] |= 1 << offset
+}
+
+func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitmap {
+	var data []uint64
+	return &uncompressedBitmap{
+		data: data,
+	}
+}
+
+func (b *uncompressedBitmap) Intersect(other *uncompressedBitmap) *uncompressedBitmap {
+	var data []uint64
+	return &uncompressedBitmap{
+		data: data,
+	}
+}
